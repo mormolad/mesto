afdf646b331f93dd466a0b7b3b21bba156fc3202
Perevalov Anton <mormolad@yandex.ru>
2023-02-09 02:19:42 +0700

    dont done 7 item

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ let usernamePopup = document.querySelector('#popup-user__username');
 let employmentPopup = document.querySelector('#popup-user__employment');
 let submitPopup = document.querySelector('#popup-user__content');
 let closePopup = document.querySelector('#popup-user__close-popup');
+const body = document.querySelector('.page');
 
 editButtom.addEventListener('click', showPopup);
 closePopup.addEventListener('click', closingPopup);
@@ -18,12 +19,17 @@ submitPopup.addEventListener('submit', submitingPopup);
 function showPopup() {
   usernamePopup.value = username.textContent;
   employmentPopup.value = employment.textContent;
-  popupUser.classList.add('popup_enable');
+  popupUser.style.opacity = 1;
+  popupUser.style.visibility = 'visible';
 }
 
 function closingPopup() {
-  popupUser.classList.remove('popup_enable');
+  popupUser.style.opacity = 0;
+  setTimeout(() => {
+    popupUser.style.visibility = 'hidden';
+  }, 1000);
 }
+
 function submitingPopup(evt) {
   evt.preventDefault();
   if (usernamePopup.value !== '' && employmentPopup.value !== '') {
@@ -49,11 +55,15 @@ closePopupAdd.addEventListener('click', closingPopupPlace);
 submitPopupAdd.addEventListener('submit', submitingPopupPlace);
 
 function showPopupPlace() {
-  popupPlace.classList.add('popup_enable');
+  popupPlace.style.opacity = 1;
+  popupPlace.style.visibility = 'visible';
 }
 
 function closingPopupPlace() {
-  popupPlace.classList.remove('popup_enable');
+  popupPlace.style.opacity = 0;
+  setTimeout(() => {
+    popupPlace.style.visibility = 'hidden';
+  }, 1000);
 }
 
 function submitingPopupPlace(evt) {
@@ -73,36 +83,56 @@ const cards = document.querySelector('.cards');
 function addCard(item) {
   const card = document.querySelector('#card-item').content;
   const insertedCard = card.querySelector('.card').cloneNode(true);
-  insertedCard.querySelector('.card__mask-card').src = item.link;
   insertedCard.querySelector('.card__mesto').textContent = item.name;
+  const image = insertedCard.querySelector('.card__mask-card');
+  image.src = item.link;
+  image.addEventListener('click', (evt) => {
+    const popupImage = document.querySelector('#popup-image').content;
+    const openPopup = popupImage.querySelector('#popup-image__overlay').cloneNode(true);
+    openPopup.querySelector('#popup-image__image-popup').src = item.link;
+    openPopup.querySelector('#popup-image__title').textContent = item.name;
+    const closeButtonPopupImage = openPopup.querySelector('#popup-image__close-popup');
+    console.log(openPopup.querySelector('#popup-image__title'));
+    closeButtonPopupImage.addEventListener('click', (evt) => {
+      openPopup.remove();
+    });
+    body.append(openPopup);
+    openPopup.style.opacity = 1;
+    openPopup.style.visibility = 'visible';
+  });
+
   cards.prepend(insertedCard);
+  const basket = document.querySelector('#card-del-card');
+  basket.addEventListener('click', (evt) => {
+    insertedCard.remove();
+  });
 }
 
 function fillWithCards() {
   const initialCards = [
     {
       name: 'Карачаево-Черкеск',
-      link: './images/karachaevsk.jpg',
+      link: './images/karachaevsk_X.jpg',
     },
     {
       name: 'Гора Эльбрус',
-      link: './images/elbrus.jpg',
+      link: './images/elbrus_X.jpg',
     },
     {
       name: 'Домбай',
-      link: './images/dombai.jpg',
+      link: './images/dombai_X.jpg',
     },
     {
       name: 'Красноярск',
-      link: './images/krasnoyarsk.jpg',
+      link: './images/krasnoyarsk_X.jpg',
     },
     {
       name: 'Петропаловск-Камчатский',
-      link: './images/petropavlovsk1.jpg',
+      link: './images/petropavlovsk_X.jpg',
     },
     {
       name: 'Эльтон',
-      link: './images/elton.jpg',
+      link: './images/elton_X.jpg',
     },
   ];
   initialCards.forEach((item) => {
@@ -115,5 +145,14 @@ const like = document.querySelectorAll('#card_like');
 like.forEach((item) => {
   item.addEventListener('click', (evt) => {
     evt.target.classList.toggle('card__like_state_active');
+  });
+});
+
+const basket = document.querySelectorAll('#card-del-card');
+
+basket.forEach((item) => {
+  let cardRemoved = item.parentNode;
+  item.addEventListener('click', (evt) => {
+    cardRemoved.remove();
   });
 });
