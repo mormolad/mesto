50f229ba61ddf6b894eff24d795cf94613cd1e4b
Perevalov Anton <mormolad@yandex.ru>
2023-03-21 06:52:00 +0700

    7spint start

--- modules/Card.js
+++ modules/Card.js
@@ -1,5 +1,42 @@
 class Card {
-  constructor(card) {
-    this.card = card;
+  constructor(item) {
+    this.name = item.name;
+    this.link = item.link;
+  }
+
+  _showPopup(element) {
+    element.classList.add('popup_enable');
+    window.addEventListener('keydown', closePopupWithKeyEsc);
+  }
+
+  _renderPopupImage(evt) {
+    const imagePopupImages = document.querySelector('#popup-image__image-popup');
+    const titlePopupImages = document.querySelector('#popup-image__title');
+    const popupImage = document.querySelector('#popup-image');
+    imagePopupImages.src = evt.srcElement.src;
+    imagePopupImages.alt = evt.srcElement.alt;
+    titlePopupImages.textContent = evt.srcElement.alt;
+    console.log(popupImage);
+    this._showPopup(popupImage);
+  }
+
+  render() {
+    const cardTemplate = document.querySelector('#card-item').content;
+    const sampleCard = cardTemplate.querySelector('.card').cloneNode(true);
+    const imageCard = sampleCard.querySelector('.card__mask-card');
+    const nameCard = sampleCard.querySelector('.card__mesto');
+    imageCard.src = this.link;
+    imageCard.alt = this.name;
+    nameCard.textContent = this.name;
+    sampleCard.querySelector('#card_like').addEventListener('click', (evt) => {
+      evt.target.classList.toggle('card__like_state_active');
+    });
+    sampleCard.querySelector('#button-del-card').addEventListener('click', () => {
+      sampleCard.remove();
+    });
+    imageCard.addEventListener('click', this._renderPopupImage);
+    return sampleCard;
   }
 }
+
+export default Card;
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,37 @@
 ﻿'use strict';
 
+import Card from '../modules/Card.js';
+const initialCards = [
+  {
+    name: 'Карачаево-Черкеск',
+    link: './images/karachaevsk_X.jpg',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus_X.jpg',
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombai_X.jpg',
+  },
+  {
+    name: 'Красноярск',
+    link: './images/krasnoyarsk_X.jpg',
+  },
+  {
+    name: 'Петропаловск-Камчатский',
+    link: './images/petropavlovsk_X.jpg',
+  },
+  {
+    name: 'Эльтон',
+    link: './images/elton_X.jpg',
+  },
+];
+const cardsContainer = document.querySelector('.cards');
+const imagePopupImages = document.querySelector('#popup-image__image-popup');
+const titlePopupImages = document.querySelector('#popup-image__title');
 
-const 
+initialCards.forEach((item) => {
+  const card = new Card(item);
+  cardsContainer.append(card.render());
+});
