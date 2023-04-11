44fa6262abcfb95b85d3ab56a1c73e102c3a472b
mormolad <mormolad@yandex.ru>
2023-04-11 20:23:22 +0700

    eee

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,8 +1,9 @@
 import Popup from './Popup.js';
 
 export default class PopupWithForm extends Popup {
-    constructor(selectorPopup, submitForm) {
-        super(selectorPopup);
-        this._form = this._popup.querySelector('#content-popup-add-card');
-    }
+  constructor(selectorPopup, submitForm) {
+    super(selectorPopup);
+    this._form = this._popup.querySelector('#content-popup-add-card');
+    this._submitForm = submitForm;
+  }
 }
