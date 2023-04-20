a9e58eee484565de9e5954331ef4a8cff71e1bb0
mormolad <mormolad@yandex.ru>
2023-04-20 16:38:15 +0700

    class done

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,33 +1,33 @@
 import Popup from './Popup.js';
 
 export default class PopupWithForm extends Popup {
-  constructor(selectorPopup, submitForm) {
-    super(selectorPopup);
-    this.form = this._popup.querySelector('.popup__content');
-    this._submitForm = submitForm;
-    this._inputs = this.form.querySelectorAll('.popup__field');
-  }
-  //собирает данные всех полей формы.
-  _getInputValues() {
-    const dataInputs = this._inputs.map((item) => {
-      //console.log(item.value);
-      return item.value;
-    });
-    return dataInputs;
-  }
-  //должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
-  setEventListeners() {
-    this.form.addEventListener('submit', (evt) => {
-      evt.preventDefault();
+    constructor(selectorPopup, submitForm) {
+            super(selectorPopup);
+            this.form = this._popup.querySelector('.popup__content');
+            this._submitForm = submitForm;
+            this._inputs = this.form.querySelectorAll('.popup__field');
+        }
+        //собирает данные всех полей формы.
+    _getInputValues() {
+            const dataInputs = this._inputs.map((item) => {
+                console.log('декампозируй код');
+                return item.value;
+            });
+            /return dataInputs;
+        }
+        //должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
+    setEventListeners() {
+            this.form.addEventListener('submit', (evt) => {
+                evt.preventDefault();
 
-      this._submitForm(evt);
-      this.close();
-    });
-    super.setEventListeners();
-  }
-  //при закрытии попапа форма должна ещё и сбрасываться.
-  close() {
-    this.form.reset();
-    super.close();
-  }
+                this._submitForm(evt);
+                this.close();
+            });
+            super.setEventListeners();
+        }
+        //при закрытии попапа форма должна ещё и сбрасываться.
+    close() {
+        this.form.reset();
+        super.close();
+    }
 }
