7c9548d4bc0dd25426d569b80c5f619ad9660216
Perevalov Anton <mormolad@yandex.ru>
2023-04-21 23:00:42 +0700

    8spint webpack done!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 ﻿'use strict';
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
-import validationOptions from '../utils/validateOptions.js';
-import initialCards from '../utils/dataCard.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
-
+import Card from './components/Card.js';
+import FormValidator from './components/FormValidator.js';
+import validationOptions from './utils/validateOptions.js';
+import initialCards from './utils/dataCard.js';
+import Section from './components/Section.js';
+import PopupWithImage from './components/PopupWithImage.js';
+import PopupWithForm from './components/PopupWithForm.js';
+import UserInfo from './components/UserInfo.js';
+import './pages/index.css';
 const buttonEdit = document.querySelector('#profile__edit-button');
 const buttonAdd = document.querySelector('#profile__add-button');
 const cardsContainer = document.querySelector('.cards');
@@ -24,31 +24,54 @@ const allCards = new Section(
   '.cards'
 );
 //создаём экземпляр класса попапа с формой для добовления места
-const popupAddCard = new PopupWithForm('#popup-add-card', handlePlaceFormSubmit);
+const popupAddCard = new PopupWithForm(
+  '#popup-add-card',
+  handlePlaceFormSubmit
+);
 const inputNamePlace = popupAddCard._popup.querySelector('#input-place-name');
-const inputUrlImagePlace = popupAddCard._popup.querySelector('#input-url-image-place');
+const inputUrlImagePlace = popupAddCard._popup.querySelector(
+  '#input-url-image-place'
+);
 //вставляем готовый элемент на страницу
 allCards.rendererCards();
 //создаём экземпляр класса для попапа с картинкой места
 const popupOpenImage = new PopupWithImage('#popup-image');
 //создаём экземпляр класса попапа с формой для редактирования профиля
-const popupEditUser = new PopupWithForm('#popup-edit-user', handleProfileFormSubmit);
+const popupEditUser = new PopupWithForm(
+  '#popup-edit-user',
+  handleProfileFormSubmit
+);
 const inputName = popupEditUser.form.querySelector('#input-user-name');
-const inputEmployment = popupEditUser.form.querySelector('#input-user-employment');
+const inputEmployment = popupEditUser.form.querySelector(
+  '#input-user-employment'
+);
 //включаем валидацию
-const validationPopupEditUser = new FormValidator(validationOptions, popupEditUser.form);
+const validationPopupEditUser = new FormValidator(
+  validationOptions,
+  popupEditUser.form
+);
 validationPopupEditUser.enableValidation();
 //включаем валидацию
-const validationPopupAddCard = new FormValidator(validationOptions, popupAddCard.form);
+const validationPopupAddCard = new FormValidator(
+  validationOptions,
+  popupAddCard.form
+);
 validationPopupAddCard.enableValidation();
-const userInfo = new UserInfo({ selectorName: '#profile__username', selectorEmployment: '#profile__employment' });
+const userInfo = new UserInfo({
+  selectorName: '#profile__username',
+  selectorEmployment: '#profile__employment',
+});
 //функция открытия попапа
 function renderPopupImage(data) {
   popupOpenImage.open(data);
 }
 // обработчик кнопки принять в форме добавления места
 function handlePlaceFormSubmit(data) {
-  const card = new Card({ name: data.name, link: data.description }, selectorTemplateCard, renderPopupImage);
+  const card = new Card(
+    { name: data.name, link: data.description },
+    selectorTemplateCard,
+    renderPopupImage
+  );
   cardsContainer.prepend(card.render());
 }
 
